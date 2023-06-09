2f42b684673e013c23f0c6c55b101072c987bd57
obaydul_Kuasha <88238461+0baydulKuasha@users.noreply.github.com>
2023-06-09 10:12:30 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.dest-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, 
+    arrows: false,
+  });
+
+
+
+  $("#counter").countMe(40,65);
+
+      
